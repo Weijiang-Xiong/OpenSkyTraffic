940f329e3ef5cffe6007c8616cd1200c97c99dc3
Weijiang Xiong <weijiangxiong1998@gmail.com>
2023-04-25 15:27:10 +0200

    simplify shape manipulations

--- engine.py
+++ engine.py
@@ -11,6 +11,7 @@ class Trainer():
                            supports=supports, 
                            in_dim=args.in_dim, 
                            out_dim=args.pred_win, 
+                           rnn_layers=args.rnn,
                            hid_dim=args.hid_dim, 
                            enc_layers=args.enc, 
                            dec_layers=args.dec, 
@@ -24,42 +25,36 @@ class Trainer():
         self.scaler = scaler
         self.clip = 3
 
-    def train(self, input, real_val):
+    def train(self, data, label):
         
         self.model.train()
         self.optimizer.zero_grad()
         
-        output = self.model(input) # out shape (N, T, M, 1)
-        output = output.transpose(1, 3) # out shape (N, 1, M, T)
-        
-        real = torch.unsqueeze(real_val,dim=1) 
+        output, _ = self.model(data) # out shape (N, M, T)
         predict = self.scaler.inverse_transform(output)
         
-        loss = self.loss(predict, real, 0.0)
+        loss = self.loss(predict, label, 0.0)
         loss.backward()
         
         if self.clip is not None:
             torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.clip)
         self.optimizer.step()
         
-        mape = util.masked_mape(predict,real,0.0).item()
-        mae = util.masked_mae(predict,real,0.0).item()
-        rmse = util.masked_rmse(predict,real,0.0).item()
+        mape = util.masked_mape(predict,label,0.0).item()
+        mae = util.masked_mae(predict,label,0.0).item()
+        rmse = util.masked_rmse(predict,label,0.0).item()
         
         return mae,mape,rmse
 
-    def eval(self, input, real_val):
+    def eval(self, data, label):
         
         self.model.eval()
         
-        output = self.model(input)
-        output = output.transpose(1, 3)
-        
-        real = torch.unsqueeze(real_val, dim=1)
+        output, _ = self.model(data)
         predict = self.scaler.inverse_transform(output)
         
-        mape = util.masked_mape(predict,real,0.0).item()
-        mae = util.masked_mae(predict,real,0.0).item()
-        rmse = util.masked_rmse(predict,real,0.0).item()
+        mape = util.masked_mape(predict,label,0.0).item()
+        mae = util.masked_mae(predict,label,0.0).item()
+        rmse = util.masked_rmse(predict,label,0.0).item()
         
         return mae,mape,rmse
--- model.py
+++ model.py
@@ -6,22 +6,30 @@
 """
 
 import torch
-import numpy as np 
+import numpy as np
 import torch.nn as nn
 
+
 class LearnedPositionalEncoding(nn.Module):
-    def __init__(self,d_model, dropout = 0.1,max_len = 500):
+    
+    def __init__(self, d_model, dropout=0.1, max_len=500, batch_first=True):
         super().__init__()
-        self.learned_pos = nn.Parameter(torch.rand(max_len, 1, d_model))
-        self.dropout = nn.Dropout(p = dropout)
+        self.batch_first = batch_first
+        # the input dimension is (batch_size, seq_len, feature_dim) if batch_first is True
+        self.batch_dim, self.att_dim = (0, 1) if batch_first else (1, 0)
+        self.encoding_dict = nn.Parameter(torch.rand(size=(max_len, d_model)))
+        self.dropout = nn.Dropout(p=dropout)
 
     def forward(self, x):
-        # modified because the previous implementation (which call .data) will lose gradient 
-        x = x + self.learned_pos[:x.size(0), ...]
+        encoding = self.encoding_dict[:x.size(self.att_dim), :].unsqueeze(self.batch_dim)
+        x = x + encoding
         return self.dropout(x)
 
+
 class PositionalEncoding(nn.Module):
-    def __init__(self, d_model, dropout=0.1, max_len=500):
+    
+    def __init__(self, d_model, dropout=0.1, max_len=500, batch_first=True):
+        
         super(PositionalEncoding, self).__init__()
         self.dropout = nn.Dropout(p=dropout)
 
@@ -30,58 +38,62 @@ class PositionalEncoding(nn.Module):
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-np.log(10000.0) / d_model))
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0).transpose(0, 1)
-        self.register_buffer('pe', pe)
-
+        self.register_buffer('encoding_dict', pe)
+        
+        self.batch_first = batch_first
+        self.batch_dim, self.att_dim = (0, 1) if batch_first else (1, 0)
+        
     def forward(self, x):
-        x = x + self.pe[:x.size(0), :]
+        encoding = self.encoding_dict[:x.size(self.att_dim), :].unsqueeze(self.batch_dim)
+        x = x + encoding
         return self.dropout(x)
 
 
 class TemproalEmbedding(nn.Module):
-    def __init__(self, in_dim, layers=1, dropout = .1):
+    def __init__(self, in_dim, layers=1, dropout=.1):
         super(TemproalEmbedding, self).__init__()
-        self.rnn = nn.LSTM(input_size=in_dim,hidden_size=in_dim,num_layers=layers,dropout=dropout)
+        self.rnn = nn.LSTM(input_size=in_dim, hidden_size=in_dim, num_layers=layers, dropout=dropout, batch_first=True)
 
     def forward(self, input):
-        ori_shape = input.shape
-        x = input.permute(3, 0, 2, 1) # (N, C, M, T) => (T, N, M, C)
-        x = x.reshape(ori_shape[3], ori_shape[0] * ori_shape[2], ori_shape[1]) # (T, N, M, C) => (T, N*M, C)
-        x,_ = self.rnn(x)
-        x = x.reshape(ori_shape[3], ori_shape[0], ori_shape[2], ori_shape[1]) # (T, N*M, C) => (T, N, M, C)
-        x = x.permute(1, 3, 2, 0) # (T, N, M, C) => (N, C, M, T)
+        N, C, M, T = input.shape
+        x = input.permute(0, 2, 3, 1)  # (N, C, M, T) => (N, M, T, C)
+        x = x.reshape(N*M, T, C)  # (N, M, T, C) => (N*M, T, C)
+        x, _ = self.rnn(x)
+        x = x.reshape(N, M, T, C)  # (N*M, T, C) => (N, M, T, C)
+        x = x.permute(0, 3, 1, 2)  # (N, M, T, C) => (N, C, M, T)
         return x
 
+
 class TrafficTransformer(nn.Module):
-    def __init__(self, in_dim, enc_layers=1, dec_layers=1, dropout=.1,heads=4):
+    def __init__(self, in_dim, enc_layers=1, dec_layers=1, dropout=.1, heads=4):
         super().__init__()
         self.heads = heads
-        self.pos = PositionalEncoding(in_dim,dropout=dropout)
+        self.pos = PositionalEncoding(in_dim, dropout=dropout)
         self.lpos = LearnedPositionalEncoding(in_dim, dropout=dropout)
-        self.trans = nn.Transformer(in_dim, heads, num_encoder_layers=enc_layers, num_decoder_layers=dec_layers, dim_feedforward=in_dim*4, dropout=dropout)
+        self.trans = nn.Transformer(in_dim, heads, num_encoder_layers=enc_layers, num_decoder_layers=dec_layers, dim_feedforward=in_dim*4, dropout=dropout, batch_first=True)
 
-    def forward(self,input, mask):
-        x = input.permute(1,0,2) # (N, M, C) => (M, N, C), batch_first=False by default 
+    def forward(self, x, mask):
         x = self.pos(x)
         x = self.lpos(x)
-        x = self.trans(x,x,tgt_mask=mask) # TODO this is probably wrong because the second input should be query (if we don't do autoregressive prediction, like DETR)
-        return x.permute(1,0,2)
+        x = self.trans(x, x, tgt_mask=mask)  # TODO this is probably wrong because the second input should be query (if we don't do autoregressive prediction, like DETR)
+        return x
 
-    def _gen_mask(self,input):
+    def _gen_mask(self, input):
         l = input.shape[1]
         mask = torch.eye(l)
         mask = mask.bool()
         return mask
 
+
 class TTNet(nn.Module):
-    def __init__(self, dropout=0.1, supports=None, in_dim=2, out_dim=12, hid_dim=32, enc_layers=6, dec_layers=6, heads=2):
+    def __init__(self, dropout=0.1, supports=None, in_dim=2, out_dim=12, rnn_layers=3, hid_dim=64, enc_layers=6, dec_layers=6, heads=2):
         super(TTNet, self).__init__()
-        self.start_conv = nn.Conv2d(in_channels=in_dim,
-                                    out_channels=hid_dim,
-                                    kernel_size=(1, 1))
-        self.start_embedding = TemproalEmbedding(hid_dim, layers=3, dropout=dropout)
-        self.end_conv = nn.Linear(hid_dim, out_dim)
-        self.end_conv_var = nn.Linear(hid_dim, out_dim)
+        self.feature_embedding = nn.Conv2d(in_channels=in_dim,
+                                           out_channels=hid_dim,
+                                           kernel_size=(1, 1))
+        self.temp_embedding = TemproalEmbedding(hid_dim, layers=rnn_layers, dropout=dropout)
+        self.mean_estimate = nn.Linear(hid_dim, out_dim)
+        self.var_estimate = nn.Linear(hid_dim, out_dim)
         self.network = TrafficTransformer(in_dim=hid_dim, enc_layers=enc_layers, dec_layers=dec_layers, dropout=dropout, heads=heads)
 
         mask = sum([s.detach() for s in supports])
@@ -90,9 +102,49 @@ class TTNet(nn.Module):
 
     def forward(self, input):
         # input shape: (N, C_in, M, T)
-        x = self.start_conv(input) # out shape (N, C_hid, M, T)
-        x = self.start_embedding(x)[..., -1] # out shape (N, C_hid, M), take the last step output
-        x = x.transpose(1, 2) # (N, C_hid, M) => (N, M, C_hid)
-        x = self.network(x, self.mask) # out shape (N, M, C_hid)
-        x = self.end_conv(x) # out shape (N, M, T)
-        return x.transpose(1,2).unsqueeze(-1) # (N, T, M, 1)
+        x = self.feature_embedding(input)  # out shape (N, C_hid, M, T)
+        x = self.temp_embedding(x)  # out shape (N, C_hid, M, T)
+        x = x[..., -1].transpose(1, 2)  # (N, C_hid, M) => (N, M, C_hid)
+        x = self.network(x, self.mask)  # out shape (N, M, C_hid)
+        mean = self.mean_estimate(x)  # out shape (N, M, T)
+        var = self.var_estimate(x)  # out shape (N, M, T)
+
+        return mean, var  # (N, M, T)
+
+def test_ttnet_forward():
+    
+    print("Testing TTNet forward pass...")
+    N, C, M, T = 8, 2, 207, 12
+    random_input = torch.rand((N, C, M, T))
+    random_support = [torch.randint(0, 2, (M, M)).bool() for _ in range(2)]
+    model = TTNet(supports=random_support)
+    mean, var = model(random_input)
+    assert mean.shape == (N, M, T)
+    assert var.shape == (N, M, T)
+    print("Test OK")
+
+def test_posenc():
+    
+    print("Testing positional encoding...")
+    
+    encoders_batch_first = [
+        PositionalEncoding(64, max_len=100, batch_first=True),
+        LearnedPositionalEncoding(64, max_len=100, batch_first=True)
+        ]
+    encoders_not_batch_first = [
+        PositionalEncoding(64, max_len=100, batch_first=False),
+        LearnedPositionalEncoding(64, max_len=100, batch_first=False)
+        ]
+    
+    for enc in encoders_batch_first:
+        print(enc.__class__.__name__)
+        out = enc(torch.ones(size=(2, 12, 64)))
+        
+    for enc in encoders_not_batch_first:
+        print(enc.__class__.__name__)
+        out = enc(torch.ones(size=(12, 2, 64)))
+
+if __name__ == "__main__":
+
+    test_posenc()
+    test_ttnet_forward()
