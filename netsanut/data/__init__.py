bf33960f42aa4e522a70ce704538a34b253bafbe
Weijiang Xiong <weijiangxiong1998@gmail.com>
2023-06-06 15:44:11 +0200

    fix import error

--- netsanut/data/__init__.py
+++ netsanut/data/__init__.py
@@ -1,4 +1,4 @@
-from .dataset import build_trainvaltest_loaders, NetworkedTimeSeriesDataset, tensor_collate, tensor_to_gpu
+from .dataset import build_trainvaltest_loaders, NetworkedTimeSeriesDataset, tensor_collate, tensor_to_contiguous
 from .transform import StandardScaler
 from .adjacency import load_adjacency
 from .catalog import DATASET_CATALOG
--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,15 +1,15 @@
-from netsanut.data import NetworkedTimeSeriesDataset, tensor_collate, tensor_to_gpu
+from netsanut.data import NetworkedTimeSeriesDataset, tensor_collate, tensor_to_contiguous
 from torch.utils.data import DataLoader
 
 dataset = NetworkedTimeSeriesDataset(compute_metadata=True)
 dataloader = DataLoader(dataset, batch_size=32, shuffle=True, drop_last=True, collate_fn=tensor_collate)
 
 for data, label in dataloader:
-    print(data.shape)
-    print(label.shape)
+    print(data.device, data.shape)
+    print(label.device, data.shape)
     break
 
-dataloader = DataLoader(dataset, batch_size=32, shuffle=True, drop_last=True, collate_fn=tensor_to_gpu)
+dataloader = DataLoader(dataset, batch_size=32, shuffle=True, drop_last=True, collate_fn=tensor_to_contiguous)
 
 for data, label in dataloader:
     print(data.device, data.shape)
