c99eda81f718c83f5b1a270bc4fc5a227264ddd2
Weijiang Xiong <weijiangxiong1998@gmail.com>
2023-08-04 11:44:55 +0200

    example script to run multiple commands

---
+++ tools/run_multiple.py
@@ -0,0 +1,35 @@
+import subprocess
+
+RUN, VISUALIZE = 0, 1
+
+mode = VISUALIZE
+
+if mode == RUN:
+    common = "python tools/train.py --config-file config/NeTSFormer_uncertainty.py"
+elif mode == VISUALIZE:
+    common = "python tools/visualize.py --result-dir"
+    
+dataset = "metr-la"
+
+command_list = [] 
+for p in [1, 2, 3]:
+    for a in [0.5, 1, 2, 3]:
+        cmd = [common]
+        if mode == RUN:
+            cmd.append("data.dataset={} train.max_epoch=40".format(dataset))
+            cmd.append(
+                "train.milestone_cfg.model.loss.exponent={} train.milestone_cfg.model.loss.alpha={}".format(p, a)
+            )
+            cmd.append(
+                "train.output_dir=scratch/uncertainty_p{}a{}_{}".format(p, a, dataset).replace("0.5", "half").replace("-", "_")
+            )
+        elif mode == VISUALIZE:
+            cmd.append(
+            "scratch/uncertainty_p{}a{}_{}".format(p, a, dataset).replace("0.5", "half").replace("-", "_")
+            )
+        cmd_str = " ".join(cmd)
+        command_list.append(cmd_str)
+
+for cmd_str in command_list:
+    print("Running command \n {}".format(cmd_str))
+    completed_process = subprocess.run(cmd_str, shell=True)
