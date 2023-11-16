ad24a82dcef1744529fc8ff8430644a1bd5060c7
Weijiang Xiong <weijiangxiong1998@gmail.com>
2023-11-16 21:35:21 +0100

    remove redundant file

--- datasets/scripts/aimsun/process_trajectories.py
+++
@@ -1,9 +0,0 @@
-import glob 
-import subprocess
-
-paths = sorted(glob.glob("/home/weijiang/Projects/Netsanut/datasets/simbarca/session_*"))
-
-for path in paths:
-    cmd = "python /home/weijiang/Projects/Netsanut/datasets/scripts/aimsun/time_series_from_traj.py --metadata_folder /home/weijiang/Projects/Netsanut/datasets/simbarca/metadata --session_folder {}".format(path)
-    print("Executing command: \n {}".format(cmd))
-    subprocess.run(cmd, shell=True)
