4443d1e99de17333f1021b7e432aa8c22a581c46
weijiang xiong <weijiangxiong1998@gmail.com>
2025-04-04 12:08:39 +0200

    fix typo

--- scripts/run_multiple.py
+++ scripts/run_multiple.py
@@ -97,9 +97,11 @@ def experiment_tf_glb(command_list):
     """ Use transformer encoder for global message exchange
     """
     command_list.append(
-    f"{train_script} {cfg_default} model.adjacency_hop=5 model.tf_glb=True train.output_dir=scratch/himsnet_5hop_norm_avgagg"
+    f"{train_script} {cfg_default} model.adjacency_hop=5 model.tf_glb=True train.output_dir=scratch/himsnet_5hop_tf_glb"
     )
-    
+    command_list.append(
+    f"{train_script} {cfg_default} model.adjacency_hop=5 model.tf_glb=False train.output_dir=scratch/himsnet_5hop_gnn_glb"
+    )
     return command_list
 
 def experiment_penetration_rate(command_list):
@@ -160,7 +162,8 @@ def experiment_gmm_model(cfg_str, command_list, note="0"):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--mode", type=str, default=None, choices=["run", "vis", "both"], help="training or visualization")
+    parser.add_argument("--mode", type=str, choices=["run", "vis", "both"], help="training or visualization")
+    parser.add_argument("--on_cluster", action="store_true", help="whether to run on cluster")
     args = parser.parse_args()
     
     mode = args.mode
@@ -171,13 +174,13 @@ if __name__ == "__main__":
     command_list = []
 
     experiment_adjacency_hops(command_list)
-    # experiment_epochs(command_list)
-    # experiment_model_sizes(command_list)
-    # experiment_data_modality(command_list)
+    experiment_epochs(command_list)
+    experiment_model_sizes(command_list)
+    experiment_data_modality(command_list)
     experiment_norm_and_attn_agg(command_list)
     experiment_tf_glb(command_list)
     experiment_penetration_rate(command_list)
-    # experiment_weight_factor(command_list)
+    experiment_weight_factor(command_list)
     # experiment_emb_ablation_repeat(command_list)
     # experiment_gmm_model("--config-file config/GMMPredRND.py", command_list, note="rndobs")
     # experiment_gmm_model("--config-file config/GMMPredFull.py", command_list, note="fullinfo")
@@ -195,6 +198,9 @@ if __name__ == "__main__":
         command_list = eval_list
     elif mode == "both":
         command_list = command_list + eval_list
+
+    if args.on_cluster:
+        command_list = [cmd_str.replace("python", "sbatch python_wrapper.sh") for cmd_str in command_list]
 
     print("Will run the following commands:")
     for cmd_str in command_list:
