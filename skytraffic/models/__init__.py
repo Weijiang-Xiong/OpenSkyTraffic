caa695a7407515380b90d40a2be3baca2fea7b11
Weijiang Xiong <weijiangxiong1998@gmail.com>
2025-07-02 23:08:19 +0200

    redefine base model

--- skytraffic/models/__init__.py
+++ skytraffic/models/__init__.py
@@ -1,6 +1,5 @@
 from .catalog import build_model, MODEL_CATALOG
 from .common import PositionalEncoding, LearnedPositionalEncoding, MLP, ValueEmbedding
-from .base import BaseModel
 from .netsformer import NeTSFormer
 from .himsnet import HiMSNet
 from .lstmgnn import LSTMGNN
--- skytraffic/models/netsformer.py
+++ skytraffic/models/netsformer.py
@@ -14,13 +14,13 @@ import logging
 import torch
 import torch.nn as nn
 
-from typing import List, Tuple, Dict, Callable
+from typing import List, Tuple, Dict, Callable, Any, Mapping
+from scipy.stats import rv_continuous, gennorm
 from einops import rearrange
 
 from ..loss import GeneralizedProbRegLoss
 from ..data import TensorDataScaler
 from .common import LearnedPositionalEncoding, PositionalEncoding
-from .base import BaseModel
 from .catalog import MODEL_CATALOG
 
 import warnings
@@ -145,7 +145,7 @@ def get_trivial_forward() -> Callable:
     return copy.deepcopy(trivial_forward)
 
 @MODEL_CATALOG.register()
-class NeTSFormer(BaseModel):
+class NeTSFormer(nn.Module):
 
     """ Networked Time Series Prediction with Transformer
 
@@ -177,7 +177,14 @@ class NeTSFormer(BaseModel):
                  temp_causal: bool = False, # add causal mask to temporal attention
                 ) -> None:
 
-        super().__init__(exponent)
+        super().__init__()
+        
+        # Initialize attributes from BaseModel
+        self.datascaler: TensorDataScaler
+        self._calibrated_intervals: Dict[float, float] = dict() 
+        self._beta: int = exponent
+        self._distribution: rv_continuous = None
+        self._set_distribution(beta=self._beta)
         
         self.sto_layers = list()
         
@@ -220,6 +227,96 @@ class NeTSFormer(BaseModel):
                 torch.ones(hist_len, hist_len, dtype=torch.bool).tril(diagonal=0)
                 ).to(self.device) if temp_causal else None
         ) # use register buffer so it will be moved to the correct device by model.to(device)
+
+    @property   
+    def device(self):
+        return list(self.parameters())[0].device
+    
+    @property
+    def num_params(self):
+        return sum([p.numel() for p in self.parameters() if p.requires_grad])
+    
+    @property
+    def is_probabilistic(self):
+        return self._distribution is not None
+
+    def forward(self, data: dict[str, torch.Tensor]):
+        """
+            time series forecasting task, 
+            data is assumed to have (N, T, M, C) shape (assumed to be unnormalized)
+            label is assumed to have (N, T, M) shape (assumed to be unnormalized)
+            
+            compute loss in training mode, predict future values in inference
+        """
+        
+        # preprocessing (if any)
+        source, target = self.preprocess(data)
+        
+        if self.training:
+            assert target is not None, "label should be provided for training"
+            return self.compute_loss(source, target)
+        else:
+            # we should not have target sequences in inference
+            return self.inference(source)
+
+    def preprocess(self, data: dict[str, torch.Tensor]) -> Tuple[Any, Any]:
+        
+        source = self.datascaler.transform(data['source'].to(self.device))
+        target = data['target'].to(self.device)
+        
+        return source, target
+
+    def _set_distribution(self, beta:int) -> rv_continuous:
+        
+        if beta is None:
+            self._distribution = None 
+        else:
+            self._distribution = gennorm(beta=int(beta))
+
+        return self._distribution
+    
+    def offset_coeff(self, 
+                    confidence:float, 
+                    return_calibrated=True
+        )-> Dict[float, float]:
+        """ 
+        compute a `k` for a Generalized Gaussian Distribution parameterized by `beta`
+        such that 0.5 * confidence_interval_width = k * std, 
+        one can later obtain the confidence interval by
+            upper_bound = mean + k * pred_std
+            lower_bound = mean - k * pred_std
+            
+        References
+            https://en.wikipedia.org/wiki/Generalized_normal_distribution
+            https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.gennorm.html
+
+        Args:
+            confidence (float): _description_
+            return_calibrated (bool, optional): _description_. Defaults to True.
+
+        Returns:
+            Dict[float,Tuple[float, float]]: a mapping from confidence score to offset coefficient
+        """
+        # use the calibrated result if it has been calibrated 
+        if return_calibrated and confidence in self._calibrated_intervals.keys():
+            interval = self._calibrated_intervals[confidence]
+        else:
+            try:
+                interval = self._distribution.interval(confidence)
+            except AttributeError:
+                logger.warning("Make sure self._distribution is properly initialized, and it has the method `interval` for confidence intervals")
+                interval = (0.0, 0.0)
+            
+        return abs(interval[0])
+    
+    def post_process(self, res):
+        # the scale of uncertainty, supposed to have the same unit as prediction
+        # so it makes sense to add them. 
+        res['sigma'] = torch.exp(res['plog_sigma']*(1.0/self._beta))
+        return res
+    
+    def update_calibration(self, intervals: Dict[float,Tuple[float, float]]):
+        self._calibrated_intervals.update(intervals)
 
     def make_pred(self, x:torch.Tensor) -> Tuple[torch.Tensor]:
         """ 
@@ -314,3 +411,18 @@ class NeTSFormer(BaseModel):
         
         return {"det": [param for _, param in det_params],
                 "sto": [param for _, param in sto_params]}
+    
+    def state_dict(self):
+        ret = super().state_dict()
+        ret["_beta"] = self._beta
+        ret["_calibrated_intervals"] = self._calibrated_intervals
+        return ret
+        
+    def load_state_dict(self, state_dict: Mapping[str, Any], strict: bool = False):
+        self._beta = state_dict["_beta"]
+        self._set_distribution(self._beta)
+        self._calibrated_intervals = state_dict["_calibrated_intervals"]
+        del state_dict["_beta"]
+        del state_dict["_calibrated_intervals"]
+        super().load_state_dict(state_dict, strict)
+        return
