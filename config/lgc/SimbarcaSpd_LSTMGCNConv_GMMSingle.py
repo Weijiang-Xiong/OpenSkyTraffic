ee6c8da584d0c5e057568373a8a6eb3cd4a5e24e
weijiang xiong <weijiangxiong1998@gmail.com>
2025-07-15 16:04:59 +0200

    fix overlapped save dir

--- config/lgc/SimbarcaSpd_LSTMGCNConv_GMMSingle.py
+++ config/lgc/SimbarcaSpd_LSTMGCNConv_GMMSingle.py
@@ -9,7 +9,7 @@ from .SimbarcaSpd_LSTMGCNConv_GMM import (
 )
 
 
-train.output_dir = "scratch/simbarcaspd_lgc"
+train.output_dir = "scratch/simbarcaspd_lgc_gmm_single"
 
 model.anchors = [0.0]
 model.sizes = [3.0]
